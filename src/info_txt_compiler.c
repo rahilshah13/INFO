ca1ca56a56815ce3b86b56e4f3bef8c84dfb851d
rahilshah13 <rahilshah360@gmail.com>
2026-04-24 02:15:04 -0700

    update via samsung galaxy z flip 7
    Refactor info.txt compiler to use OpenMP for parallel processing of units. Removed unused token types and simplified the main function.

--- src/info_txt_compiler.c
+++ src/info_txt_compiler.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
-typedef enum { TOKEN_NEWLINE, TOKEN_INDENT, TOKEN_SPACE, TOKEN_COMMA, TOKEN_HYPHEN, TOKEN_ARROW, TOKEN_WORD } TokenType;
+typedef enum { TOKEN_NEWLINE, TOKEN_ARROW, TOKEN_HYPHEN, TOKEN_WORD } TokenType;
 
-void expand_stacked_macros(const char* op) {
-    if (strcmp(op, "++") == 0) {
-        printf("Macro Expansion: Stacked Increment\n");
-    } else if (strcmp(op, "--") == 0) {
-        printf("Macro Expansion: Stacked Decrement\n");
+const char* SYMBOLS[] = {"INCREMENT", "DECREMENT", "ARROW"};
+const char* MINIFIED[] = {"++", "--", "->"};
+
+void process_unit(const char* unit, int unit_id) {
+    int tid = omp_get_thread_num();
+    
+    if (strstr(unit, "++")) printf("[Thread %d] Unit %d: Macro Stacked Inc\n", tid, unit_id);
+    
+    printf("[Thread %d] Minified IR %d: ", tid, unit_id);
+    for (int i = 0; i < 3; i++) {
+        if (strstr(unit, SYMBOLS[i])) printf("%s ", MINIFIED[i]);
     }
+    printf("\n");
 }
 
-void process_token(TokenType type, const char* value) {
-    switch(type) {
-        case TOKEN_NEWLINE: printf("[Token: Newline]\n"); break;
-        case TOKEN_INDENT:  printf("[Token: Indent]\n"); break;
-        case TOKEN_ARROW:   printf("[Token: Arrow ->]\n"); break;
-        case TOKEN_HYPHEN:  printf("[Token: Hyphen -]\n"); break;
-        default: printf("Value: %s\n", value);
-    }
-}
+int main() {
+    const char* closures[] = {
+        "INCREMENT unit-alpha -> result",
+        "DECREMENT unit-beta",
+        "INCREMENT unit-gamma -> effect"
+    };
+    int total_units = 3;
 
-int main(int argc, char *argv[]) {
-    printf("--- info.txt Compiler Starting ---\n");
-    FILE *file = fopen("info.txt", "r");
-    if (!file) {
-        perror("Error opening info.txt");
-        return 1;
+    printf("--- info.txt Parallel Compiler (OpenMP) ---\n");
+    printf("Processing %d units on %d cores...\n\n", total_units, omp_get_num_procs());
+
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < total_units; i++) {
+        process_unit(closures[i], i);
     }
 
-    // Basic logic to demonstrate "Newline is a token"
-    char c;
-    while ((c = fgetc(file)) != EOF) {
-        if (c == '\n') process_token(TOKEN_NEWLINE, NULL);
-        else if (c == ' ') process_token(TOKEN_SPACE, " ");
-        else if (c == '-') {
-            char next = fgetc(file);
-            if (next == '>') process_token(TOKEN_ARROW, "->");
-            else { ungetc(next, file); process_token(TOKEN_HYPHEN, "-"); }
-        }
-    }
-
-    fclose(file);
     return 0;
 }
