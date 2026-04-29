cee221af145129bc113af7d1f7cd6ab99f3ed299
rahilshah13 <rahilshah360@gmail.com>
2026-04-29 07:46:20 -0700

    Delete verify_hashes.sh

--- verify_hashes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# verify_hashes.sh
-
-echo "### Valid Hashes ###"
-sha256sum ./INFO > ./artifacts_volume/latest_build.hash
-date >> ./artifacts_volume/latest_build.hash
-echo "Hashes verified and stored in artifacts_volume/."
