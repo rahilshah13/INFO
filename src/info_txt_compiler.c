84f0309fb47f45725ca9a341cc74c1f98ff10bea
rahilshah13 <rahilshah360@gmail.com>
2026-07-23 18:48:09 -0700

    add metrics

--- src/info_txt_compiler.c
+++ src/info_txt_compiler.c
@@ -22,20 +22,24 @@ typedef struct { IndexEntry *entries; size_t count, capacity; } InfoIndex;
 
 const char *IPA[] = {"p","b","t","d","k","g","m","n","ŋ","f","v","θ","ð","s","z","ʃ","ʒ","h","tʃ","dʒ","w","j","r","l","i","ɪ","e","ɛ","æ","a","ə","ʌ","u","ʊ","o","ɔ","ɑ","ɒ","aɪ","eɪ","ɔɪ","aʊ","oʊ"};
 
-typedef struct { int pos; uint8_t depth; int transitive; int sourceType; int nodeComplexity; } Metrics;
+typedef struct { int pos; uint8_t depth; int transitive; int sourceType; int nodeComplexity; float avgWordLen; float wordsPerClosure; int correctSentences; int incorrectSentences; } Metrics;
 
-Metrics get_metrics(const char *w) {
-    Metrics m = {3, 1, 0, 0, 0};
-    char cmd[512];
-    snprintf(cmd, 512, "tpl -l predicate.pl -l words.pl -g \"(entry('%s', P, _, _) -> (is_transitive('%s') -> T=1; T=0), phrase_depth('%s', D), (is_module('%s') -> S=1; S=0), decl_count('%s', C), format('~w,~w,~w,~w,~w', [P, T, D, S, C]); write('x,0,1,0,0')), halt.\" 2>/dev/null", w, w, w, w, w);
+Metrics get_metrics(const char *w, const char *closure_text) {
+    Metrics m = {3, 1, 0, 0, 0, 0.0f, 0.0f, 0, 0};
+    char cmd[1024];
+    snprintf(cmd, 1024, "tpl -l predicate.pl -l words.pl -g \"(entry('%s', P, _, _) -> (is_transitive('%s') -> T=1; T=0), phrase_depth('%s', D), (is_module('%s') -> S=1; S=0), decl_count('%s', C), avg_word_length('%s', AW), words_per_closure('%s', WC), sentence_eval('%s', Cor, Inc), format('~w,~w,~w,~w,~w,~w,~w,~w,~w', [P, T, D, S, C, AW, WC, Cor, Inc]); write('x,0,1,0,0,0.0,0.0,0,0')), halt.\" 2>/dev/null", w, w, w, w, w, closure_text, closure_text, closure_text);
     FILE *fp = popen(cmd, "r");
-    if (fp) { char r[64]; if (fgets(r, 64, fp)) {
-        char *t[5]; int i=0; t[0]=strtok(r,","); while(i<4) t[++i]=strtok(NULL,",");
+    if (fp) { char r[256]; if (fgets(r, 256, fp)) {
+        char *t[9]; int i=0; t[0]=strtok(r,","); while(i<8) t[++i]=strtok(NULL,",");
         if(t[0]) { if(*t[0]=='n') m.pos=0; else if(*t[0]=='v') m.pos=1; else if(*t[0]=='a') m.pos=2; }
         if(t[1]) m.transitive = atoi(t[1]);
         if(t[2]) m.depth = (uint8_t)atoi(t[2]);
         if(t[3]) m.sourceType = atoi(t[3]);
         if(t[4]) m.nodeComplexity = atoi(t[4]);
+        if(t[5]) m.avgWordLen = (float)atof(t[5]);
+        if(t[6]) m.wordsPerClosure = (float)atof(t[6]);
+        if(t[7]) m.correctSentences = atoi(t[7]);
+        if(t[8]) m.incorrectSentences = atoi(t[8]);
     } pclose(fp); }
     return m;
 }
@@ -56,15 +60,21 @@ void fill_ipa(ClosureSlice c, float *d, size_t n) {
 }
 
 Tensor gen_tensor(ClosureSlice c, const char *id, float tfidf) {
-    Tensor t = {calloc(436, sizeof(float)), calloc(2, sizeof(size_t)), 2, id};
-    t.shape[0] = 1; t.shape[1] = 436;
+    Tensor t = {calloc(442, sizeof(float)), calloc(2, sizeof(size_t)), 2, id};
+    t.shape[0] = 1; t.shape[1] = 442;
     char w[64] = {0}; for(size_t i=0; i<c.length && i<63 && !isspace(c.start[i]); i++) w[i] = tolower(c.start[i]);
-    Metrics m = get_metrics(w);
+    char *c_text = malloc(c.length + 1);
+    memcpy(c_text, c.start, c.length);
+    c_text[c.length] = '\0';
+    Metrics m = get_metrics(w, c_text);
+    free(c_text);
     for (int i = 0; i < 300; i++) t.data[i] = ((float)rand()/(float)RAND_MAX) * (m.depth > 1 ? 1.5f : 1.0f);
     t.data[300 + m.pos] = 1.0f; t.data[303] = (float)m.depth;
     t.data[304] = (float)m.transitive; t.data[305] = tfidf;
     t.data[306] = (float)m.sourceType; t.data[307] = (float)m.nodeComplexity;
-    fill_bpe(c, &t.data[308], 64); fill_ipa(c, &t.data[372], 64);
+    t.data[308] = m.avgWordLen; t.data[309] = m.wordsPerClosure;
+    t.data[310] = (float)m.correctSentences; t.data[311] = (float)m.incorrectSentences;
+    fill_bpe(c, &t.data[312], 64); fill_ipa(c, &t.data[376], 64);
     return t;
 }
 
